181c360f269f44bd0da7ed29af4f139b54e4ae86
slabnevitch <handofschpilrain@mail.ru>
2022-08-18 14:47:57 +0500

    combine many examplars

--- js/libs/tabs-accordion-combine/tabs-accordion.js
+++ js/libs/tabs-accordion-combine/tabs-accordion.js
@@ -1,187 +1,202 @@
-var combine = function() {
-    var targetContent = document.querySelectorAll('.combine .tab-content'),
-        targetContainers = document.querySelectorAll('.combine .accordion__content'),
-        htmlArr = [];
+(function() {
 
-    tabsHandler = function(checkPoint){
-      var tabs = Array.prototype.slice.call(document.querySelectorAll('.tab')),
-        tabsItems = Array.prototype.slice.call(document.querySelectorAll('.tab-content')),
-        checkpoint =  checkPoint !== undefined ?  checkPoint : null;
-    
-    bindEvents = function(){
-      tabs.forEach(function(el){
-        el.addEventListener('click', clickHandler);
-      });
-    }
-    clickHandler = function(e){
-      // if(checkpoint && screen.width <=  checkPoint){
-      //   return;
-      // }
+  var  points = {
+    lg: 1199,
+    md: 991,
+    sm: 767.98,
+    xs: 576,
+    xss: 480,
+    xsss: 375,
+    min: 360,
+    mins: 320
+  };
 
-      e.preventDefault();
-      var target = e.target.closest('.tab'),
-        parent = target.closest('._tabs'),
-        items = parent.querySelectorAll('.tab-content'),
-        siblings = parent.querySelectorAll('.tab');
+  var Combine = function(combine) {
+      var targetContent = combine.querySelectorAll('.tab-content'),
+          targetContainers = combine.querySelectorAll('.accordion__content'),
+          htmlArr = [],
+          _self = this;
+
+      this.tabsHandler = function(checkPoint){
+        var tabs = Array.prototype.slice.call(combine.querySelectorAll('.tab')),
+          tabsItems = Array.prototype.slice.call(combine.querySelectorAll('.tab-content')),
+          checkpoint =  checkPoint !== undefined ?  checkPoint : null;
       
-      console.log(target.textContent)
-      // target.classList.add('active');
+         tabs.forEach(function(el){
+          el.addEventListener('click', _self.clickHandler);
+        });
+       }
 
-      for(var i=0; i<siblings.length; i++){
-        if(siblings[i] == target){
-          //alert(i);
-          siblings[i].classList.add('active')
-          items[i].classList.remove('hidden');
+      this.clickHandler = function(e){
+        // if(checkpoint && screen.width <=  checkPoint){
+        //   return;
+        // }
+        e.preventDefault();
+        
+        var target = e.target.closest('.tab'),
+          parent = target.closest('._tabs'),
+          items = parent.querySelectorAll('.tab-content'),
+          siblings = parent.querySelectorAll('.tab');
+        
+        // target.classList.add('active');
 
-        }else{
-          siblings[i].classList.remove('active')
-          items[i].classList.add('hidden');
-        }
+        for(var i=0; i<siblings.length; i++){
+          if(siblings[i] == target){
+            //alert(i);
+            siblings[i].classList.add('active')
+            items[i].classList.remove('hidden');
 
-      }
-    }
-    return {
-      bindEvents
-    }
-  }
+          }else{
+            siblings[i].classList.remove('active')
+            items[i].classList.add('hidden');
+          }
 
-   accordion = function(checkPoint){
-    var links = document.querySelectorAll('.accordion__link'),
-    duration = 500;
-    
-    init = function(){
-      for(var i=0 ; i<links.length; i++){
-        links[i].addEventListener('click', clicker);  
-      }
-    },
-    clicker = function(e){
-      if(screen.width >= checkPoint){
-        return;
-      }
-      e.preventDefault();
-      var content = this.nextElementSibling,
-      accordionList = e.target.closest('.accordion');
-      slideToggle(content, duration);
-
-      if(accordionList.hasAttribute('data-accordion')){
-        slideSiblings(this, duration);
-      }
-    },
-    slideSiblings = function(item){
-      var parent = item.parentNode;
-      var accordionChildren = item.closest('.accordion__item').parentNode.children;
-      
-      for(var i=0; i< accordionChildren.length; i++){
-        if(accordionChildren[i] !== parent  && accordionChildren[i].querySelector('.accordion__content') !== null){
-          slideUp(accordionChildren[i].querySelector('.accordion__content'), duration);
         }
       }
-    },
-    slideUp = function(target, duration){
-      target.style.transitionProperty = 'height, margin, padding';
-      target.style.transitionDuration = duration + 'ms';
-      target.style.boxSizing = 'border-box';
-      target.style.height = target.offsetHeight + 'px';
-      target.offsetHeight;
-      target.style.overflow = 'hidden';
-      target.style.height = 0;
-      target.style.paddingTop = 0;
-      target.style.paddingBottom = 0;
-      target.style.marginTop = 0;
-      target.style.marginBottom = 0;
-      window.setTimeout( function(){
-        target.classList.add('hidden');
-        target.style.removeProperty('height');
+    // }
+
+     this.accordion = function(checkPoint){
+      var links = combine.querySelectorAll('.accordion__link'),
+      duration = 500;
+      
+      init = function(){
+        for(var i=0 ; i<links.length; i++){
+          links[i].addEventListener('click', clicker);  
+        }
+      },
+      clicker = function(e){
+        if(screen.width >= checkPoint){
+          return;
+        }
+        e.preventDefault();
+        var content = this.nextElementSibling,
+        accordionList = e.target.closest('.accordion');
+        slideToggle(content, duration);
+
+        if(accordionList.hasAttribute('data-accordion')){
+          slideSiblings(this, duration);
+        }
+      },
+      slideSiblings = function(item){
+        var parent = item.parentNode;
+        var accordionChildren = item.closest('.accordion__item').parentNode.children;
+        
+        for(var i=0; i< accordionChildren.length; i++){
+          if(accordionChildren[i] !== parent  && accordionChildren[i].querySelector('.accordion__content') !== null){
+            slideUp(accordionChildren[i].querySelector('.accordion__content'), duration);
+          }
+        }
+      },
+      slideUp = function(target, duration){
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.boxSizing = 'border-box';
+        target.style.height = target.offsetHeight + 'px';
+        target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        window.setTimeout( function(){
+          target.classList.add('hidden');
+          target.style.removeProperty('height');
+          target.style.removeProperty('padding-top');
+          target.style.removeProperty('padding-bottom');
+          target.style.removeProperty('margin-top');
+          target.style.removeProperty('margin-bottom');
+          target.style.removeProperty('overflow');
+          target.style.removeProperty('transition-duration');
+          target.style.removeProperty('transition-property');
+        }, duration);
+        target.parentNode.classList.remove('active');
+      },
+      slideDown = function(target, duration){
+        target.classList.remove('hidden');
+        let isHidden = target.classList.contains('hidden');
+        if (isHidden) target.classList.remove('hidden');;
+        let height = target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        target.offsetHeight;
+        target.style.boxSizing = 'border-box';
+        target.style.transitionProperty = "height, margin, padding";
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = height + 'px';
         target.style.removeProperty('padding-top');
         target.style.removeProperty('padding-bottom');
         target.style.removeProperty('margin-top');
         target.style.removeProperty('margin-bottom');
-        target.style.removeProperty('overflow');
-        target.style.removeProperty('transition-duration');
-        target.style.removeProperty('transition-property');
-      }, duration);
-      target.parentNode.classList.remove('active');
-    },
-    slideDown = function(target, duration){
-      target.classList.remove('hidden');
-      let isHidden = target.classList.contains('hidden');
-      if (isHidden) target.classList.remove('hidden');;
-      let height = target.offsetHeight;
-      target.style.overflow = 'hidden';
-      target.style.height = 0;
-      target.style.paddingTop = 0;
-      target.style.paddingBottom = 0;
-      target.style.marginTop = 0;
-      target.style.marginBottom = 0;
-      target.offsetHeight;
-      target.style.boxSizing = 'border-box';
-      target.style.transitionProperty = "height, margin, padding";
-      target.style.transitionDuration = duration + 'ms';
-      target.style.height = height + 'px';
-      target.style.removeProperty('padding-top');
-      target.style.removeProperty('padding-bottom');
-      target.style.removeProperty('margin-top');
-      target.style.removeProperty('margin-bottom');
-      window.setTimeout( function(){
-        target.style.removeProperty('height');
-        target.style.removeProperty('overflow');
-        target.style.removeProperty('transition-duration');
-        target.style.removeProperty('transition-property');
-      }, duration);
-      target.parentNode.classList.add('active');
-    },
-    slideToggle = function(target, duration){
-      if (target.classList.contains('hidden')) {
-        return slideDown(target, duration);
-      } else {
-        return slideUp(target, duration);
+        window.setTimeout( function(){
+          target.style.removeProperty('height');
+          target.style.removeProperty('overflow');
+          target.style.removeProperty('transition-duration');
+          target.style.removeProperty('transition-property');
+        }, duration);
+        target.parentNode.classList.add('active');
+      },
+      slideToggle = function(target, duration){
+        if (target.classList.contains('hidden')) {
+          return slideDown(target, duration);
+        } else {
+          return slideUp(target, duration);
+        }
       }
+      init();
     }
-    init();
-  }
 
-  adaptive = function(checkPoint) {
-    
-    Array.prototype.slice.call(targetContent).forEach(function(item) {
-       htmlArr.push(item.querySelector('.replaced-tab-content'));
-    });
+    this.adaptive = function(checkPoint) {
+      Array.prototype.slice.call(targetContent).forEach(function(item) {
+         htmlArr.push(item.querySelector('.replaced-tab-content'));
+      });
 
-    var mediaQuery = window.matchMedia('(max-width:' + checkPoint + 'px)');
+      var mediaQuery = window.matchMedia('(max-width:' + checkPoint + 'px)');
 
-    function mediaQueryChange(e) {
-      if (e.matches) {
-        // console.log('Media Query Matched!')
-        replaceContent();
+      function mediaQueryChange(e) {
+        if (e.matches) {
+          // console.log('Media Query Matched!')
+          _self.replaceContent();
 
-      }else{
-        // console.log('Media Query NOT Matched!')
-        returnContent();
+        }else{
+          // console.log('Media Query NOT Matched!')
+          _self.returnContent();
+        }
       }
+      mediaQuery.addListener(mediaQueryChange);
+      mediaQueryChange(mediaQuery);
     }
-    mediaQuery.addListener(mediaQueryChange);
-    mediaQueryChange(mediaQuery);
-  }
 
-  replaceContent = function() {
-    htmlArr.forEach(function(item, i) {
-      targetContainers[i].insertAdjacentElement('beforeend', item);
-    });
-  }
-  returnContent = function() {
-     targetContainers.forEach(function(item, i) {
-      targetContent[i].insertAdjacentElement('beforeend', htmlArr[i]);
-    });
-  }
-
-  return{
-    init: function(checkPoint){
-      adaptive(checkPoint);
-      tabsHandler(checkPoint).bindEvents();
-      accordion(checkPoint);
+    this.replaceContent = function() {
+      htmlArr.forEach(function(item, i) {
+        targetContainers[i].insertAdjacentElement('beforeend', item);
+      });
     }
+    this.returnContent = function() {
+       targetContainers.forEach(function(item, i) {
+        targetContent[i].insertAdjacentElement('beforeend', htmlArr[i]);
+      });
+    }
+
+    // return{
+    this.init = function(elem){
+        var checkPoint = points[elem.dataset.point];
+         
+        this.adaptive(checkPoint);
+        this.tabsHandler(checkPoint);
+        this.accordion(checkPoint);
+      }
+    // }
   }
-}
-//Call:
-if(document.querySelector('.combine') !== null){
-  combine().init(991); // аргумент checkpoint должен соответствовать $checkpoint в _tabs-accordion.scss
-}
+  //Call:
+  if(document.querySelector('.combine') !== null){
+    Array.prototype.slice.call(document.querySelectorAll('.combine'))
+      .forEach(function(elem) {
+       new Combine(elem).init(elem); // аргумент checkpoint должен соответствовать $checkpoint в _tabs-accordion.scss
+      });
+  }
+})();
